db9b406d82c8ff2220d3ca4ca133e26c8de680a9
theWornOut <ekremalpayy@gmail.com>
2018-03-11 17:22:10 +0300

    Abstract class, interfaces

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -2,9 +2,6 @@
 {
     using System;
 
-    // Interface ile Virtual Metotların birleşimidir.
-    // Tamamen inheritance amaçlı kullanılır.
-    // Interfaces gibi abstract classes new'lenmez. Instance'i alınmaz.
     internal class Program
     {
         private static void Main(string[] args)
@@ -21,14 +18,11 @@
 
     abstract class Database
     {
-        // Abstract classes ile tamamlanmış ve tamamlanmamış metotlar oluşturmak mümkün.
-        // Interfaces gibidir. Tek bir sınıfa inheritance edilir.
         public void Add()
         {
             Console.WriteLine("Added by default");
         }
 
-        // İçi dolu olmayan virtual metotdur.
         public abstract void Delete();
     }
 
