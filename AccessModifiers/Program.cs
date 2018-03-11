8211a1b4274eed125e9d95051c1b9dc74b963ec5
theWornOut <ekremalpayy@gmail.com>
2018-03-11 17:27:40 +0300

    Use of access modifiers. Private, protected, public, internal

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -7,15 +7,9 @@
         }
     }
 
-    // Default Internal
-    // Bağlı bulunduğu proje içerisinde referans eklemeden kullanılır.
     internal class Customer
     {
-        // Sadece tanımlanan yerden çağırılır.
         private int Id;
-
-        // Private'ın bütün özelliklerini kapsar.
-        // Tanımlanan özellik/değişken vs. inheritance(kalıtım) edildiği sınıflarında kullanılır.
         protected int Id2;
 
         public void Save()
