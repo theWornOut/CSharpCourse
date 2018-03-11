abec26a8655a844cb9c09e5f3dc09fd470d656af
theWornOut <ekremalpayy@gmail.com>
2018-03-11 21:18:53 +0300

    Reference and value type, reference assignment

---
+++ ReferenceAndValueType/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ReferenceAndValueType
+{
+    using System;
+    using System.Data;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            number2 = number1;
+            number1 = 30;
+            Console.WriteLine(number2);
+
+            string[] cities1 = { "Istanbul", "Ankara" };
+            string[] cities2 = { "Antalya", "Bursa" };
+            cities2 = cities1;
+            cities1[0] = "İzmir";
+            Console.WriteLine(cities2[0]);
+
+            DataTable dataTable1;
+            DataTable dataTable2 = new DataTable();
+            dataTable1 = dataTable2;
+
+            Console.ReadLine();
+        }
+    }
+}
