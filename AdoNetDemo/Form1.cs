5646cb14711d7958c48bdc6e7582d0562228bf25
theWornOut <ekremalpayy@gmail.com>
2018-03-13 18:08:43 +0300

    Ado.Net Crud Operations

---
+++ AdoNetDemo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AdoNetDemo
+{
+    public partial class Form1 : Form
+    {
+        ProductDal _productDal = new ProductDal();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            dgvProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = txtName.Text,
+                UnitPrice = Convert.ToDecimal(txtUnitPrice.Text),
+                StockAmount = Convert.ToInt32(txtStockAmount.Text)
+            });
+            LoadProducts();
+            MessageBox.Show("Product Added!");
+        }
+
+        private void dgvProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvProducts.CurrentRow != null)
+            {
+                txtIdUpdate.Text = dgvProducts.CurrentRow.Cells[0].Value.ToString();
+                txtNameUpdate.Text = dgvProducts.CurrentRow.Cells[1].Value.ToString();
+                txtUnitPriceUpdate.Text = dgvProducts.CurrentRow.Cells[2].Value.ToString();
+                txtStockAmountUpdate.Text = dgvProducts.CurrentRow.Cells[3].Value.ToString();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.CurrentRow != null)
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(dgvProducts.CurrentRow.Cells[0].Value),
+                    Name = txtNameUpdate.Text,
+                    UnitPrice = Convert.ToDecimal(txtUnitPriceUpdate.Text),
+                    StockAmount = Convert.ToInt32(txtStockAmountUpdate.Text)
+                };
+                _productDal.Update(product);
+            }
+            LoadProducts();
+            MessageBox.Show("Updated!");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvProducts.CurrentRow.Cells[0].Value);
+                _productDal.Delete(id);
+            }
+            LoadProducts();
+            MessageBox.Show("Deleted!");
+        }
+    }
+}
---
+++ AdoNetDemo/ProductDal.cs
@@ -0,0 +1,95 @@
+﻿namespace AdoNetDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Data;
+
+    public class ProductDal
+    {
+        SqlConnection _connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB; Initial Catalog=ETrade; Integrated Security=True;");
+        public List<Product> GetAll()
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("select * from products", _connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"])
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            _connection.Close();
+
+            return products;
+        }
+
+        public DataTable GetAllDataTable()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+            SqlCommand command = new SqlCommand("select * from products", _connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            _connection.Close();
+
+            return dataTable;
+        }
+
+        private void ConnectionControl()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void Add(Product product)
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("insert into products values(@Name, @UnitPrice, @StockAmount)", _connection);
+            command.Parameters.AddWithValue("@Name", product.Name);
+            command.Parameters.AddWithValue("@UnitPrice", product.UnitPrice);
+            command.Parameters.AddWithValue("@StockAmount", product.StockAmount);
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+        }
+
+        public void Update(Product product)
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("update products set Name=@Name, UnitPrice=@UnitPrice, StockAmount=@StockAmount where Id=@Id", _connection);
+            command.Parameters.AddWithValue("@Id", product.Id);
+            command.Parameters.AddWithValue("@Name", product.Name);
+            command.Parameters.AddWithValue("@UnitPrice", product.UnitPrice);
+            command.Parameters.AddWithValue("@StockAmount", product.StockAmount);
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+        }
+
+        public void Delete(int id)
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("delete from products where Id=@Id", _connection);
+            command.Parameters.AddWithValue("@Id", id);
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+        }
+    }
+}
