d539ec0a57a247e0319dd5d8f02a67b4db2c76f3
theWornOut <ekremalpayy@gmail.com>
2018-03-14 15:01:52 +0300

    Func

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -7,8 +7,44 @@
     {
         private static void Main(string[] args)
         {
-            ExceptionIntro();
+            //ExceptionIntro();
 
+            //TryCatch();
+
+            //ActionDemo();
+
+            // in, in, out result
+            Func<int, int, int> add = Add;
+            Console.WriteLine(add(3, 5));
+            Console.WriteLine(Add(2, 3));
+
+            // out result
+            Func<int> getRandomNumber = delegate
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+            Console.WriteLine(getRandomNumber);
+
+            // Without Parameter - Use to Lambda
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+            Console.WriteLine(getRandomNumber2);
+
+            Console.ReadLine();
+        }
+
+        private static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static void ActionDemo()
+        {
+            HandleException(() => { Find(); });
+        }
+
+        private static void TryCatch()
+        {
             try
             {
                 Find();
@@ -17,13 +53,6 @@
             {
                 Console.WriteLine(exception);
             }
-
-            HandleException(() =>
-            {
-                Find();
-            });
-
-            Console.ReadLine();
         }
 
         private static void HandleException(Action action)
