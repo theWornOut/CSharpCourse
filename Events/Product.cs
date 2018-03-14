56baa8649a7d7b60f358218740293417fbb32129
theWornOut <ekremalpayy@gmail.com>
2018-03-14 15:14:07 +0300

    Events

---
+++ Events/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace Events
+{
+    using System;
+
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stock;
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int Stock
+        {
+            get => _stock;
+            set
+            {
+                _stock = value;
+                if (value <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent?.Invoke();
+                }
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine($"{ProductName} Stock amount {Stock}");
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Events
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Product harddiskProduct = new Product(50) { ProductName = "Hard Disk" };
+            Product gsmProduct = new Product(50) { ProductName = "GSM" };
+            gsmProduct.StockControlEvent += GsmProduct_StockControlEvent;
+
+            for (int i = 0; i < 10; i++)
+            {
+                harddiskProduct.Sell(10);
+                gsmProduct.Sell(10);
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void GsmProduct_StockControlEvent()
+        {
+            Console.WriteLine("Gsm about to finish!");
+        }
+    }
+}
